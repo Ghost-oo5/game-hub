ead3ba082cad3aa885c4d2b48631e2b1ffc55ac5
Ghost-oo5 <rajaabdulbasit005@gmail.com>
2024-09-26 14:23:13 +0500

    Displayed Critic Score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import { Game } from "../Hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -13,7 +14,10 @@ export default function GameCard({ game }: Props) {
         <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
-        <PlatformIconList platform={game.parent_platforms.map((item)=>item.platform)}/>
+        <HStack justifyContent={"space-between"}>
+          <PlatformIconList platform={game.parent_platforms.map((item)=>item.platform)}/>
+            <CriticScore score={game.metacritic}/>
+        </HStack>
       </CardBody>
       </Card>
     </>
