27f2c0f7548a8db7c1ded751c1af7fec5ea2f93e
Ghost-oo5 <rajaabdulbasit005@gmail.com>
2024-09-27 22:52:58 +0500

    Added Search Input

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,14 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
-import logo from "../assets/logo.png"
-import "../index.css"
+import logo from "../assets/logo.png";
+import "../index.css";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 export default function Navbar() {
   return (
-    <HStack justifyContent={"space-between"} padding={"10px"}>
-        <Image src={logo} boxSize={'60px'} className="logo"/>
-        <Text>Ghosts.co</Text>
-        <ColorModeSwitch/>
+    <HStack  padding={"10px"}>
+      <Image src={logo} boxSize={"60px"} className="logo" />
+      <SearchInput />
+      <ColorModeSwitch />
     </HStack>
-  )
+  );
 }
