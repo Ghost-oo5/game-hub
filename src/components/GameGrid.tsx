a0cfb852b463c66bcb07aca01db5059af9c17205
Ghost-oo5 <rajaabdulbasit005@gmail.com>
2024-09-28 17:39:56 +0500

    Refactored GameGrid code

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,7 +10,8 @@ interface Props {
 }
 export default function GameGrid({ gameQuery }: Props) {
   const { data, error, isLoading } = useGames(gameQuery);
-  const skeletons = [1, 2, 3, 4, 5, 6];
+  const skeletons = [1, 2, 3, 4, 5, 6,7,8,9,10];
+  if(error) return <Text>{error}</Text>;
   return (
     <>
       {error && (
